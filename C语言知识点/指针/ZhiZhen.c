e83ea4229ad1269bafb4b9e7207e5b91594b3968
HangFan6 <2691567181@qq.com>
2023-01-19 19:28:55 +0800

    指针

---
+++ C语言知识点/指针/ZhiZhen.c
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS 1  //解决不安全访问警告/报错问题
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<string.h>
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int* p = arr;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", *p);
+//		p = p + 1;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	printf("%d\n", & arr[9] - &arr[0]);
+//	return 0;
+//}
+
+//int my_strlen(char* str)
+//{
+//	char* start = str;
+//	char* end = str;
+//	while (*end != '\0')
+//		end++;
+//	return end - start;
+//}
+//int main()
+//{
+//	char arr[] = "bit";
+//	int len = my_strlen(arr);
+//	printf("%d\n", len);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	printf("%p\n", arr);//首元素地址
+//	printf("%p\n", &arr[0]);
+//	printf("%p\n", &arr);//整个数组的地址（从首元素地址开始）
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int* p = arr;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%p ===== %p\n", p + i, &arr[i]);
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		*(p + i) = i;
+//		printf("%d ", *(p + i));// *(p + i)=arr[i]
+//	}
+//	return 0;
+//}
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	int* arr[3] = { &a,&b,&c };
+	int i = 0;
+	for (i = 0; i < 3; i++)
+	{
+		printf("%d ", *(arr[i]));
+	}
+	return 0;
+}
