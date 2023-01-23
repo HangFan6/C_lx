d98d74bb49c4b494ad4242c2a4a102d077ba175a
HangFan6 <2691567181@qq.com>
2023-01-24 02:51:24 +0800

    指针

--- C语言进阶/指针/ZhiZhen.c
+++ C语言进阶/指针/ZhiZhen.c
@@ -4,21 +4,152 @@
 #include<stdlib.h>
 #include<string.h>
 
-void delay(unsigned int i)
-{
-	unsigned int j;
-	for (; i > 0; i--)
-		for (j = 0; j < 125; j++)
-		{
-			;
-		}
-}
+//int main()
+//{
+//	char* p = "abcdef";//"abcdef"是一个常量字符串，不能被修改
+//	//最好写为const char* p
+//	*p = 'W';//存在段错误
+//	printf("%s\n", p);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr1[] = "abcdef";
+//	char arr2[] = "acbdef";
+//	char* p1 = "abcdef";
+//	char* p2 = "abcdef";
+//	if (arr1 == arr2)
+//	{
+//		printf("hehe\n");
+//	}
+//	else
+//	{
+//		printf("haha\n");
+//	}
+//	if (p1 == p2)
+//	{
+//		printf("hehe\n");
+//	}
+//	else
+//	{
+//		printf("haha\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10, b = 20, c = 30;
+//	int* arr[3] = { &a,&b,&c };
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("%d ", *(arr[i]));
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5 };
+//	int arr2[] = { 2,3,4,5,6 };
+//	int arr3[] = { 3,4,5,6,7 };
+//	int* parr[] = { arr1,arr2,arr3 };
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 5; j++)
+//		{
+//			printf("%d ", *(parr[i] + j));
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int* p = NULL;//p是整形指针 - 指向整形的指针 - 可以存放整形的地址
+//	char* pc = NULL;//pc是字符指针 - 指向字符的指针 - 可以存放字符的地址
+//	//数组指针 - 指向数组的指针 - 存放数组的地址
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	//arr - 首元素地址
+//	//&arr[0] - 首元素地址
+//	//&arr - 数组的地址
+//
+//	//int* p[10]由于[]的优先级高于*，认为p是一个数组，用于存放指针（数组指针）
+//	int (*p)[10] = &arr;//存放数组的地址
+//	//先说明p是一个指针，指向数组，类型为整形
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	//方法1：（最优）
+//	int* p = arr;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//	//方法2-A：
+//	//int (*p)[10] = &arr;
+//	//int i = 0;
+//	//for (i = 0; i < 10; i++)
+//	//{
+//	//	printf("%d ", *(*p+i));//*p == arr
+//	//}
+//	//方法2-B：
+//	/*for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", (*p)[i]);
+//	}*/
+//	return 0;
+//}
+
+////参数是数组的形式
+//void print1(int arr[3][5], int x, int y)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < x; i++)
+//	{
+//		for (j = 0; j < y; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	printf("\n");
+//}
+////参数是指针的形式
+//void print2(int (*p)[5], int x, int y)
+//{
+//	int i = 0;
+//	for (i = 0; i < x; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < y; j++)
+//		{
+//			printf("%d ", *(*(p + i) + j));//*(*(p+i)+j)等于(*(p+i))[j]
+//			//p+i是找到某一行的地址，*(p+i)进行解引用
+//		}
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int arr[3][5] = { {1,2,3,4,5},{2,3,4,5,6},{3,4,5,6,7} };
+//	print1(arr, 3, 5);//arr - 数组名 - 是首元素地址（二维数组中是第一行数组地址）
+//	print2(arr, 3, 5);
+//	return 0;
+//}
+
 int main()
 {
-	while (1)
-	{
-		printf("hehe!!!\n");
-		delay(5000000);
-	}
+
 	return 0;
 }
