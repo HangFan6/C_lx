fb826e11c5f092aa3d7d86783d600b62624ad187
HangFan6 <2691567181@qq.com>
2023-01-23 21:45:08 +0800

    指针

---
+++ C语言进阶/指针/ZhiZhen.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS  //解决不安全访问警告/报错问题
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<string.h>
+
+void delay(unsigned int i)
+{
+	unsigned int j;
+	for (; i > 0; i--)
+		for (j = 0; j < 125; j++)
+		{
+			;
+		}
+}
+int main()
+{
+	while (1)
+	{
+		printf("hehe!!!\n");
+		delay(5000000);
+	}
+	return 0;
+}
