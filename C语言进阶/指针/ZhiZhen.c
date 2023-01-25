6fd2969f04b54cd3d151f672d18dd5d08101b1e5
HangFan6 <2691567181@qq.com>
2023-01-25 14:30:32 +0800

    指针

--- C语言进阶/指针/ZhiZhen.c
+++ C语言进阶/指针/ZhiZhen.c
@@ -256,16 +256,155 @@
 //	return 0;
 //}
 
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	//printf("%d\n", Add(a, b));
+//	printf("%p\n", &Add);
+//	printf("%p\n", Add);
+//	//&函数名 和 函数名 都是函数的地址
+//	return 0;
+//}
+
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int(*p)(int, int) = Add;
+//	//此处函数变量名可写，可不写 int(*p)(int x, int y) = Add;
+//	printf("%d\n", p(2, 3));//p是一个函数的地址，可以直接调用函数
+//	printf("%d\n", (*p)(2, 3));
+//	return 0;
+//}
+
+//void Print(char* str)
+//{
+//	printf("%s\n", str);
+//}
+//int main()
+//{
+//	void (*p)(char*) = Print;
+//	(*p)("hello bit!");
+//	return 0;
+//}
+
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int Sub(int x, int y)
+//{
+//	return x - y;
+//}
+//int Mul(int x, int y)
+//{
+//	return x * y;
+//}
+//int Div(int x, int y)
+//{
+//	return x / y;
+//}
+//int main()
+//{
+//	//需要一个数组，可以存放4个函数的地址--函数指针的数组
+//	int(*p)(int, int) = Add;//Add,Sub,Mul,Div
+//	int(*parr[4])(int, int) = { Add,Sub,Mul,Div };//函数指针的数组
+//	int i = 0;
+//	for (i = 0; i < 4; i++)
+//	{
+//		printf("%d\n", parr[i](2, 3));//parr可以不用解引用
+//		//i=0时，调用Add；i=1时，调用Sub；......
+//	}
+//}
+
+//char* my_strcpy(char* dest, const char* src);
+////写一个函数指针pf，能够指向 my_strcpy
+////写一个函数指针的数组，能够存放4个my_strcpy
+//char* (*pf)(char*, const char*);
+//char* (*parr[4])(char*, const char*);
+
+//计算器实现
+void menu()
+{
+	printf("***********************\n");
+	printf("**** 1.Add  2.Sub  ****\n");
+	printf("**** 3.Mul  4.Div  ****\n");
+	printf("****     0.Exit    ****\n");
+	printf("***********************\n");
+}
 int Add(int x, int y)
 {
-	int z = 0;
-	z = x + y;
-	return z;
+	return x + y;
+}
+int Sub(int x, int y)
+{
+	return x - y;
+}
+int Mul(int x, int y)
+{
+	return x * y;
+}
+int Div(int x, int y)
+{
+	return x / y;
 }
 int main()
 {
-	int a = 10;
-	int b = 20;
-	printf("%d\n", Add(a, b));
+	int input = 0;
+	int x = 0, y = 0;
+	do
+	{
+		menu();
+		printf("请选择：");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			printf("请输入两个操作数：");
+			scanf("%d%d", &x, &y);
+			printf("%d\n", Add(x, y));
+			break;
+		case 2:
+			printf("请输入两个操作数：");
+			scanf("%d%d", &x, &y);
+			printf("%d\n", Sub(x, y));
+			break;
+		case 3:
+			printf("请输入两个操作数：");
+			scanf("%d%d", &x, &y);
+			printf("%d\n", Mul(x, y));
+			break;
+		case 4:
+			printf("请输入两个操作数：");
+			scanf("%d%d", &x, &y);
+			printf("%d\n", Div(x, y));
+			break;
+		case 0:
+			printf("退出计算\n");
+			break;
+		default:
+			printf("选择错误\n");
+			break;
+		}
+	} while (input);
 	return 0;
 }
+
+//int main()
+//{
+//
+//	return 0;
+//}
